0e9d28d9a9fea63a0fe12fb53ba60e1e610be307
Marcel Grünauer <marcel@cpan.org>
2007-11-07 09:59:16 +0000

    Import of MARCEL/Class-Value-Net-0.01 from CPAN.
    gitpan-cpan-distribution: Class-Value-Net
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCEL/Class-Value-Net-0.01.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

---
+++ inc/Test/CompanionClasses.pm
@@ -0,0 +1,36 @@
+#line 1
+package Test::CompanionClasses;
+
+use warnings;
+use strict;
+use Test::CompanionClasses::Engine;
+use Getopt::Long;
+
+
+use base 'Exporter';
+
+
+our $VERSION = '0.02';
+
+
+our @EXPORT = ('run_tests');
+
+
+sub run_tests {
+    my $exact;
+    GetOptions(exact => \$exact) or
+        die "usage: $0 [ --exact ] filter...\n";
+
+    Test::CompanionClasses::Engine->new->run_tests(
+        exact     => $exact,
+        filter    => [ @main::ARGV ],
+        # inherited => [ $inherited_spec ],
+    );
+}
+
+
+
+__END__
+
+#line 108
+
